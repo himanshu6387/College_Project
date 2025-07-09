5dd797bd5d0463e8ba0922ce3b47f6f5d9f40553
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-07-09 15:51:38 +0530

    updated one

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <footer className="bg-dark text-white text-center w-100 position-fixed bottom-0 mt-3 p-2">
+            <footer className="bg-dark text-white text-center w-100 position-fixed bottom-0 mt-5 p-2">
                 <div className="container">
                     <p className="mb-0">
                         &copy; 2025 <a href="https://allaroundaid.in" className="text-decoration-none text-white fw-bold">AllAroundAid.in</a>. All rights reserved.
