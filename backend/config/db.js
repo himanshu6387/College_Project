498e39d309772101c766318429465635fd8afced
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-09-22 11:39:23 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect('mongodb+srv://abhiranjanms5646:8aCB8YwojAA5jq6z@cluster1.3acwtwk.mongodb.net/college-form?retryWrites=true&w=majority&appName=Cluster1')
         console.log('MongoDB Connected Successfully..')
     } catch (error) {
         console.log(error)
